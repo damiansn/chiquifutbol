c0952d82cad1725ccf6349bd4836cb419ccbeb19
Damian Armani <beeralbum.info@gmail.com>
2026-09-14 22:42:07 -0300

    agregar filtro de calendario de tu equipo

--- src/app/page.js
+++ src/app/page.js
@@ -633,7 +633,7 @@ export default function Home() {
         >
           <input
             type="text"
-            placeholder="🔍 Buscar equipo (ej. River, Boca...)"
+            placeholder="🔍 ¿Cúando juega mi equipo?"
             value={
               teamSearchQuery
             }
