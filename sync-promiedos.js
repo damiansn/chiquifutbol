bec70533b70b54110d8eab156648a6fad0744dab
Damian Armani <beeralbum.info@gmail.com>
2026-09-16 13:45:58 -0300

    agregar filtro de calendario de tu equipo

--- sync-promiedos.js
+++ sync-promiedos.js
@@ -1379,18 +1379,25 @@ const standingsData = {
     return false;
 
   }
+console.log("==========================================");
 console.log("DEBUG STANDINGS DATA");
-console.log("brackets existe:", Array.isArray(standingsData.brackets));
+console.log("==========================================");
+
+console.log("brackets existe:", !!standingsData.brackets);
+console.log("brackets tipo:", typeof standingsData.brackets);
 console.log(
-  "brackets cantidad:",
-  Array.isArray(standingsData.brackets)
-    ? standingsData.brackets.length
-    : "NO ARRAY"
+  "brackets stages:",
+  standingsData.brackets?.stages?.length ?? 0
 );
-console.log(
-  "claves standingsData:",
-  Object.keys(standingsData)
-);
+
+if (standingsData.brackets?.stages) {
+  console.log(
+    "etapas:",
+    standingsData.brackets.stages.map(stage => stage.name)
+  );
+}
+
+console.log("claves standingsData:", Object.keys(standingsData));
 
 
   // ==========================================
