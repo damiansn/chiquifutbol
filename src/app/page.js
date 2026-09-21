d3a2da55c4988765adebc9e37ea8512ecfdf2dc5
Damian Armani <beeralbum.info@gmail.com>
2026-09-21 16:22:27 -0300

    agregar escudos equipos argentinos

--- src/app/page.js
+++ src/app/page.js
@@ -401,7 +401,7 @@ export default function Home() {
       <div style={S.navbar}>
         <div style={S.navInner}>
           <a href="/" style={{ ...S.navLogo, display: "flex", alignItems: "center", gap: "8px" }}>
-            <img src="/logo.svg" alt="ChiquiFútbol" width="28" height="28" style={{ objectFit: "contain" }} onError={e => { e.currentTarget.style.display = "none"; }} />
+            <img src="/logo.svg" alt="ChiquiFútbol" width="48" height="48" style={{ objectFit: "contain" }} onError={e => { e.currentTarget.style.display = "none"; }} />
             Secanuca
           </a>
         </div>
